bd9a8d2cfa80cc09fc26e127ba2c30dda6c3cea7
Luke Jackson <lukejacksonn@gmail.com>
2018-01-20 19:54:27 +0000

    0.11.0

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-var r=function(a){var e=a||[],o=e[0],n=e[1],t=e[2];return!!a&&("object"!=typeof n||Array.isArray(n)?r([o,{},n]):{name:o,props:n,children:Array.isArray(t)?t.map(r).filter(Boolean):t+""})};export{r as h};
+var r,t=function(a){return function(e){return!!e&&("object"!=typeof e[1]||Array.isArray(e[1])?t(a)([e[0],{},e[1]]):((r={})[a[0]]=e[0],r[a[1]]=e[1],r[a[2]]=Array.isArray(e[2])?e[2].map(t(a)).filter(Boolean):e[2]+"",r))}};export{t as h};
 //# sourceMappingURL=index.js.map
