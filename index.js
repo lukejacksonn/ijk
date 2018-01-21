b0a618d9cd034678725c444c2e34bb856cb73309
Luke Jackson <lukejacksonn@gmail.com>
2018-01-21 16:50:34 +0000

    Just expose configurable h

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const clense = (a, b) =>
   !b ? a : typeof b[0] === 'string' ? [...a, b] : [...a, ...b]
 
-const factory = (x, y, z) => node =>
+export const h = (x, y, z) => node =>
   !!node && typeof node[1] === 'object' && !Array.isArray(node[1])
     ? {
         [x]: node[0],
         [y]: node[1],
         [z]: Array.isArray(node[2])
-          ? node[2].reduce(clense, []).map(factory(x, y, z))
+          ? node[2].reduce(clense, []).map(h(x, y, z))
           : node[2] + '',
       }
-    : factory(x, y, z)([node[0], {}, node[1] || ''])
-
-export const h = factory('name', 'props', 'children')
-export const p = factory('nodeName', 'attributes', 'children')
+    : h(x, y, z)([node[0], {}, node[1] || ''])
