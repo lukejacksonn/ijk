a8208cedb789d3de3884403ef7ccd390e05cce7b
Luke Jackson <lukejacksonn@gmail.com>
2018-01-21 18:08:42 +0000

    Fixes h reference error for node imports

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const clense = (a, b) =>
   !b ? a : typeof b[0] === 'string' ? [...a, b] : [...a, ...b]
 
-export const h = (x, y, z) => node =>
+const build = (x, y, z) => node =>
   !!node && typeof node[1] === 'object' && !Array.isArray(node[1])
     ? {
         [x]: node[0],
         [y]: node[1],
         [z]: Array.isArray(node[2])
-          ? node[2].reduce(clense, []).map(h(x, y, z))
+          ? node[2].reduce(clense, []).map(build(x, y, z))
           : node[2] + '',
       }
-    : h(x, y, z)([node[0], {}, node[1] || ''])
+    : build(x, y, z)([node[0], {}, node[1] || ''])
+
+export const h = build
