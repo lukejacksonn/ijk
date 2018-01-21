f38a73b5362cc905af70a0468c0c79c8045ded39
Luke Jackson <lukejacksonn@gmail.com>
2018-01-21 14:36:37 +0000

    Reverts factory

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
-var r,t=function(r,t){return t?"string"==typeof t[0]?r.concat([t]):r.concat(t):r},e=function(n){return function(o){return!!o&&("object"!=typeof o[1]||Array.isArray(o[1])?e(n)([o[0],{},o[1]||""]):((r={})[n[0]]=o[0],r[n[1]]=o[1],r[n[2]]=Array.isArray(o[2])?o[2].reduce(t,[]).map(e(n)):o[2]+"",r))}},n=e(["name","props","children"]),o=e(["nodeName","attributes","children"]);exports.h=n,exports.p=o;
+var r=function(r,e){return e?"string"==typeof e[0]?r.concat([e]):r.concat(e):r},e=function(t){return!!t&&("object"!=typeof t[1]||Array.isArray(t[1])?e([t[0],{},t[1]||""]):{name:t[0],props:t[1],children:Array.isArray(t[2])?t[2].reduce(r,[]).map(e):t[2]+""})},t=function(e){return!!e&&("object"!=typeof e[1]||Array.isArray(e[1])?t([e[0],{},e[1]||""]):{nodeName:e[0],attributes:e[1],children:Array.isArray(e[2])?e[2].reduce(r,[]).map(t):e[2]+""})};export{e as h,t as p};
 //# sourceMappingURL=index.js.map
+p
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const clense = (a, b) =>
   !b ? a : typeof b[0] === 'string' ? [...a, b] : [...a, ...b]
 
-const factory = schema => node =>
+export const h = node =>
   !node
     ? false
     : typeof node[1] === 'object' && !Array.isArray(node[1])
       ? {
-          [schema[0]]: node[0],
-          [schema[1]]: node[1],
-          [schema[2]]: Array.isArray(node[2])
-            ? node[2].reduce(clense, []).map(factory(schema))
+          name: node[0],
+          props: node[1],
+          children: Array.isArray(node[2])
+            ? node[2].reduce(clense, []).map(h)
             : node[2] + '',
         }
-      : factory(schema)([node[0], {}, node[1] || ''])
+      : h([node[0], {}, node[1] || ''])
 
-export const h = factory(['name', 'props', 'children'])
-export const p = factory(['nodeName', 'attributes', 'children'])
+export const p = node =>
+  !node
+    ? false
+    : typeof node[1] === 'object' && !Array.isArray(node[1])
+      ? {
+          nodeName: node[0],
+          attributes: node[1],
+          children: Array.isArray(node[2])
+            ? node[2].reduce(clense, []).map(p)
+            : node[2] + '',
+        }
+      : p([node[0], {}, node[1] || ''])
