6fc7d2989c0e57ec0e366f97cf8498925c4da08b
Luke Jackson <lukejacksonn@gmail.com>
2018-01-21 15:41:00 +0000

    Tries arguments as schema

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -9,21 +9,21 @@
   var clense = function(a, b) {
     return !b ? a : typeof b[0] === 'string' ? a.concat([b]) : a.concat(b)
   }
-  var factory = function(schema) {
+  var factory = function(x, y, z) {
     return function(node) {
       return !!node && typeof node[1] === 'object' && !Array.isArray(node[1])
         ? ((obj = {}),
-          (obj[schema[0]] = node[0]),
-          (obj[schema[1]] = node[1]),
-          (obj[schema[2]] = Array.isArray(node[2])
+          (obj[x] = node[0]),
+          (obj[y] = node[1]),
+          (obj[z] = Array.isArray(node[2])
             ? node[2].reduce(clense, []).map(factory(schema))
             : node[2] + ''),
           obj)
         : factory(schema)([node[0], {}, node[1] || ''])
     }
   }
-  var h = factory(['name', 'props', 'children'])
-  var p = factory(['nodeName', 'attributes', 'children'])
+  var h = factory('name', 'props', 'children')
+  var p = factory('nodeName', 'attributes', 'children')
 
   exports.h = h
   exports.p = p
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const clense = (a, b) =>
   !b ? a : typeof b[0] === 'string' ? [...a, b] : [...a, ...b]
 
-const factory = schema => node =>
+const factory = (x, y, z) => node =>
   !!node && typeof node[1] === 'object' && !Array.isArray(node[1])
     ? {
-        [schema[0]]: node[0],
-        [schema[1]]: node[1],
-        [schema[2]]: Array.isArray(node[2])
+        [x]: node[0],
+        [y]: node[1],
+        [z]: Array.isArray(node[2])
           ? node[2].reduce(clense, []).map(factory(schema))
           : node[2] + '',
       }
     : factory(schema)([node[0], {}, node[1] || ''])
 
-export const h = factory(['name', 'props', 'children'])
-export const p = factory(['nodeName', 'attributes', 'children'])
+export const h = factory('name', 'props', 'children')
+export const p = factory('nodeName', 'attributes', 'children')
