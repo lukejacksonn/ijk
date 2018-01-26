a1f3f21d86593f12e0430409e0d87e9eea14350c
Luke Jackson <lukejacksonn@gmail.com>
2018-01-26 21:32:48 +0000

    Renames tests

--- index.test.js
+++ index.test.js
@@ -1,24 +1,40 @@
 import { h } from './index'
 
 const hh = h('x', 'y', 'z')
+const noop = x => x
 
 export default {
-  x: [hh(['hr']), { x: 'hr', y: {}, z: [] }],
-  xy: [
+  Element: [
+    hh(['button', { onclick: noop }, 'testing']),
+    { x: 'button', y: { onclick: noop }, z: 'testing' },
+  ],
+  'Element (no children)': [
     hh(['input', { type: 'range' }]),
     { x: 'input', y: { type: 'range' }, z: [] },
   ],
-  xyz: [
-    hh(['button', { onclick: console.log }, 'testing']),
-    { x: 'button', y: { onclick: console.log }, z: 'testing' },
+  'Element (no props)': [
+    hh(['button', 'testing']),
+    { x: 'button', y: {}, z: 'testing' },
   ],
-  xz: [hh(['button', 'testing']), { x: 'button', y: {}, z: 'testing' }],
-  xzx: [
-    hh(['main', [['hr']]]),
-    { x: 'main', y: {}, z: [{ x: 'hr', y: {}, z: [] }] },
+  'Element (no props, no children)': [hh(['hr']), { x: 'hr', y: {}, z: [] }],
+  'Nested Element': [
+    hh(['main', [['button', { onclick: noop }, 'testing']]]),
+    {
+      x: 'main',
+      y: {},
+      z: [{ x: 'button', y: { onclick: noop }, z: 'testing' }],
+    },
   ],
-  xzxy: [
+  'Nested Element (no children)': [
     hh(['main', [['input', { type: 'range' }]]]),
     { x: 'main', y: {}, z: [{ x: 'input', y: { type: 'range' }, z: [] }] },
+  ],
+  'Nested Element (no props)': [
+    hh(['main', [['button', 'testing']]]),
+    { x: 'main', y: {}, z: [{ x: 'button', y: {}, z: 'testing' }] },
+  ],
+  'Nested Element (no props, no children)': [
+    hh(['main', [['hr']]]),
+    { x: 'main', y: {}, z: [{ x: 'hr', y: {}, z: [] }] },
   ],
 }
