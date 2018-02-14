79f1e3ee07efc449f860aa07b2f744a727b049a7
Luke Jackson <lukejacksonn@gmail.com>
2018-02-14 19:22:52 +0000

    0.18.0

--- index.test.js
+++ index.test.js
@@ -12,6 +12,8 @@ const Component = ({ title, story, related }) => [
 const Main = [
   'main',
   [
+    'Hello',
+    ['p', 0],
     ['h1', 'Hello World'],
     ['input', { type: 'range' }],
     ['ul', [1, 2, 3].map(Item)],
@@ -28,6 +30,7 @@ const Main = [
 const Button = ['button', { onclick: noop }, 'testing']
 const Input = ['input', { type: 'range' }]
 const Title = ['h1', 'testing']
+const Zero = ['p', 0]
 const Hr = ['hr']
 
 export default {
@@ -36,6 +39,8 @@ export default {
     'no children': [hh(Input), { x: 'input', y: { type: 'range' }, z: [] }],
     'no props': [hh(Title), { x: 'h1', y: {}, z: 'testing' }],
     'no props, no children': [hh(Hr), { x: 'hr', y: {}, z: [] }],
+    'children 0': [hh(Zero), { x: 'p', y: {}, z: '0' }],
+    'just string': [hh('Hello'), 'Hello'],
   },
   Nested: {
     base: [
@@ -44,6 +49,14 @@ export default {
         x: 'main',
         y: {},
         z: [{ x: 'button', y: { onclick: noop }, z: 'testing' }],
+      },
+    ],
+    'base with string': [
+      hh(['main', [Button, 'Hello']]),
+      {
+        x: 'main',
+        y: {},
+        z: [{ x: 'button', y: { onclick: noop }, z: 'testing' }, 'Hello'],
       },
     ],
     'no children': [
@@ -66,6 +79,8 @@ export default {
         x: 'main',
         y: {},
         z: [
+          'Hello',
+          { x: 'p', y: {}, z: '0' },
           { x: 'h1', y: {}, z: 'Hello World' },
           { x: 'input', y: { type: 'range' }, z: [] },
           {
